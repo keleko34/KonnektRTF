1b884fe45926b563525e54f39a0150a421ec7c12
keleko34 <gregory.guidero@gmail.com>
2017-04-28 01:52:41 +0200

    add hashRouterListeners

--- KonnektRTF/KonnektRTF.js
+++ KonnektRTF/KonnektRTF.js
@@ -2,21 +2,60 @@ define([],function(){
   function CreaKonnektRTF()
   {
     var _base = '',
-        _onChange = function(){};
+        _onChange = function(){},
+        _hashlisteners = [],
+        _onEvent = function(e)
+        {
+          for(var x=0,len=_hashlisteners.length;x<len;x++)
+          {
+            if(!e._stopPropogation) _hashlisteners[x](e);
+          }
+          return e._preventDefault;
+        }
 
     function KonnektRTF(name)
     {
       if(name.length === 0) name = _base;
-
-      document.body.innerHTML = "<"+name+"></"+name+">";
-      _onChange(name);
+      var e = new eventObject(name);
+      if(!_onEvent(e))
+      {
+        document.body.innerHTML = "<"+e.hash+"></"+e.hash+">";
+        _onChange(e.hash);
+      }
       return KonnektRTF;
+    }
+    
+    function eventObject(hash)
+    {
+      this.preventDefault = function(){this._preventDefault = true;}
+      this.stopPropogation = function(){this._stopPropogation = true;}
+      this.hash = hash;
+      this.url = window.location.href;
     }
 
     KonnektRTF.base = function(v)
     {
       if(v === undefined) return _base;
       _base = (typeof v === 'string' ? v : _base);
+      return KonnektRTF;
+    }
+    
+    KonnektRTF.addHashListener = function(func)
+    {
+      _hashlisteners.push(func);
+      return KonnektRTF;
+    }
+    
+    KonnektRTF.removeHashListener = function(func)
+    {
+      for(var x=0,len=_hashlisteners.length;x<len;x++)
+      {
+        if(_hashlisteners[x].toString() === func.toString())
+        {
+          _hashlisteners.splice(x,1);
+          break;
+        }
+      }
       return KonnektRTF;
     }
 
